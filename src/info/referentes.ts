cde9f6ac238b4dfc297acc22bb35be7b3f404106
Jehu Frayle <jehufrayle@jehu-tuf.localdomain>
2023-10-18 21:42:50 -0700

    Added projects carrousel. Remove Lisa Su as referent, I'm sorry Lisa :(

--- src/info/referentes.ts
+++ src/info/referentes.ts
@@ -19,9 +19,9 @@ export const referentes: Referent[] = [{
     "name": "Satoru Iwata",
     "URL": "https://en.wikipedia.org/wiki/Satoru_Iwata",
     "imgURL": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Satoru_Iwata_-_Game_Developers_Conference_2011_-_Day_2_%281%29.jpg/800px-Satoru_Iwata_-_Game_Developers_Conference_2011_-_Day_2_%281%29.jpg?20110326211638"
-},
+}/*,
 {
     "name": "Lisa Su",
     "URL": "https://twitter.com/lisasu/",
     "imgURL": "https://pbs.twimg.com/profile_images/836654735553777664/gWyrS-2a_400x400.jpg"
-}]
+}*/]
